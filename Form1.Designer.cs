21865fab3982a2d998a6dca093cbb5849a1068cc
Gergan Gospodinov <gergan.gospodinov@bournegroup.ltd>
2023-03-02 17:31:49 +0000

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace FilteringApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGrid = new Tekla.Structures.Dialog.UIControls.DataGrid();
+            this.FilterAnd = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGrid
+            // 
+            this.dataGrid.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGrid.BackgroundColor = System.Drawing.Color.White;
+            this.dataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGrid.Location = new System.Drawing.Point(12, 12);
+            this.dataGrid.Name = "dataGrid";
+            this.dataGrid.Size = new System.Drawing.Size(474, 222);
+            this.dataGrid.TabIndex = 0;
+            // 
+            // FilterAnd
+            // 
+            this.FilterAnd.Location = new System.Drawing.Point(252, 240);
+            this.FilterAnd.Name = "FilterAnd";
+            this.FilterAnd.Size = new System.Drawing.Size(234, 53);
+            this.FilterAnd.TabIndex = 1;
+            this.FilterAnd.Text = "Filter \"OR\"";
+            this.FilterAnd.UseVisualStyleBackColor = true;
+            this.FilterAnd.Click += new System.EventHandler(this.FilterOr_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 240);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(234, 53);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Filter \"AND\"";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.FilterAnd_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(499, 301);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.FilterAnd);
+            this.Controls.Add(this.dataGrid);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Tekla.Structures.Dialog.UIControls.DataGrid dataGrid;
+        private System.Windows.Forms.Button FilterAnd;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections;
+using System.Data;
+using System.IO;
+using System.Windows.Forms;
+using Tekla.Structures.Filtering;
+using Tekla.Structures.Filtering.Categories;
+using Tekla.Structures.Model;
+using Tekla.Structures.Model.UI;
+using ModelObjectSelector = Tekla.Structures.Model.UI.ModelObjectSelector;
+
+namespace FilteringApp
+{
+    public partial class Form1 : Form
+    {
+        private string filterName;
+        private readonly DataTable selectionTable = new DataTable();
+        public Form1()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.filterName = "TestFilterGG";
+            this.selectionTable.Columns.Add("Name", typeof(string));
+            this.selectionTable.Columns.Add("Value", typeof(string));
+
+            var mos = new ModelObjectSelector();
+            var moe = mos.GetSelectedObjects();
+
+            var dt = new DataTable();
+            dt.Columns.Add("Name", typeof(string));
+            dt.Columns.Add("Value", typeof(string));
+
+            while (moe.MoveNext())
+            {
+                var part = moe.Current as Part;
+                if (part == null)
+                {
+                    continue;
+                }
+
+                var ht = new Hashtable();
+                part.GetAllUserProperties(ref ht);
+
+                var enumerator = ht.GetEnumerator();
+                while (enumerator.MoveNext())
+                {
+
+                    if (enumerator.Key.ToString().Contains("initial_GUID"))
+                    {
+                        continue;
+                    }
+
+                    if (enumerator.Key.ToString().Contains("FIRE_RATING"))
+                    {
+                        continue;
+                    }
+
+                    if (enumerator.Key.ToString().Contains("RFI"))
+                    {
+                        if (enumerator.Key.ToString().Contains("RFIcombined"))
+                        {
+                            dt.Rows.Add(enumerator.Key, enumerator.Value);
+                        }
+                        continue;
+                    }
+
+                    if (enumerator.Value.ToString() == "0")
+                    {
+                        continue;
+                    }
+
+                    if (enumerator.Value.ToString() == "-2147483648")
+                    {
+                        continue;
+                    }
+
+                    dt.Rows.Add(enumerator.Key, enumerator.Value);
+                }
+            }
+
+            dt = dt.DefaultView.ToTable(true);
+            dt.DefaultView.Sort = "Name, Value";
+            this.dataGrid.DataSource = dt;
+            this.dataGrid.RowHeadersVisible = false;
+            this.dataGrid.AllowUserToResizeRows = false;
+            this.dataGrid.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            this.dataGrid.AllowUserToAddRows = false;
+            this.dataGrid.ReadOnly = true;
+            this.dataGrid.ColumnHeadersVisible = false;
+        }
+
+        public void CreateFilter(string filterName, BinaryFilterOperatorType type)
+        {
+            var collection = new BinaryFilterExpressionCollection();
+
+            foreach (DataRow row in this.selectionTable.Rows)
+            {
+                var template = new TemplateFilterExpressions.CustomString(row["Name"].ToString());
+                var value = new StringConstantFilterExpression($"\"{row["Value"]}\"");
+                var expresion = new BinaryFilterExpression(template, StringOperatorType.IS_EQUAL, value);
+                var item = new BinaryFilterExpressionItem(expresion, type);
+                collection.Add(item);
+            }
+
+            var Filter = new Filter(collection);
+            var fileName = Path.Combine(@".\attributes", filterName);
+            Filter.CreateFile(FilterExpressionFileType.OBJECT_GROUP_VIEW, fileName);
+        }
+
+        private void ChangeRepresentation(string representation)
+        {
+            var VisibleViews = ViewHandler.GetVisibleViews();
+            while (VisibleViews.MoveNext())
+            {
+                var CurrentView = VisibleViews.Current;
+                CurrentView.ViewFilter = representation;
+                CurrentView.Modify();
+            }
+        }
+
+        private void CollectSelectedRows()
+        {
+            var selectedRows = this.dataGrid.SelectedRows;
+            foreach (DataGridViewRow row in selectedRows)
+            {
+                var name = row.Cells[0].Value.ToString();
+                var value = row.Cells[1].Value.ToString();
+                this.selectionTable.Rows.Add(name, value);
+            }
+        }
+
+        private void FilterOr_Click(object sender, EventArgs e)
+        {
+            this.CollectSelectedRows();
+            this.CreateFilter(this.filterName, BinaryFilterOperatorType.BOOLEAN_OR);
+            this.ChangeRepresentation(this.filterName);
+            this.selectionTable.Clear();
+        }
+
+        private void FilterAnd_Click(object sender, EventArgs e)
+        {
+            this.CollectSelectedRows();
+            this.CreateFilter(this.filterName, BinaryFilterOperatorType.BOOLEAN_AND);
+            this.ChangeRepresentation(this.filterName);
+            this.selectionTable.Clear();
+        }
+    }
+}
